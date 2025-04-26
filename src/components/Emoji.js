753981b604c95ce7d6c09fefd56b646b34997461
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-26 12:21:49 +0300

    addded an emoji function

---
+++ src/components/Emoji.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./Emoji.css";
+
+const emojiList = [
+  { emoji: "😀", name: "grinning" },
+  { emoji: "😂", name: "joy" },
+  { emoji: "😍", name: "heart eyes" },
+  { emoji: "😎", name: "sunglasses" },
+  { emoji: "😢", name: "crying" },
+  { emoji: "👍", name: "thumbs up" },
+  { emoji: "🔥", name: "fire" },
+  { emoji: "❤️", name: "heart" },
+  { emoji: "🎉", name: "party" },
+];
+
+function EmojiSearch() {
+
+}
+
